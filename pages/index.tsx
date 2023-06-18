904b132629d5b6fbf99df2f151e3ab8cb487de5d
unknown <grigorischneider@gmail.com>
2023-06-18 16:06:06 +0200

    Fetch product query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
-import play from "@/playground";
+import type { InferGetStaticPropsType } from "next";
+import getAllproducts from "../framework/shopify/product/get-all-products";
 
-export default function Home() {
-  useEffect(() => {
-    play();
-  }, []);
+export async function getStaticProps() {
+  const products = await getAllproducts();
 
-  return (
-    <>
-      <h1>HI hi</h1>
-    </>
-  );
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <>{JSON.stringify(products)}</>;
 }
